f99e905b5b5b43ed09a543c514ef5e6369056a73
vaishnavinkm <vaishnavinikam1999@gmail.com>
2025-03-09 15:05:25 -0400

    Getting the book review based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,10 +47,10 @@ public_users.get('/title/:title',function (req, res) {
         res.send(book);
 });
 
-//  Get book review
+//  Task:5 Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
