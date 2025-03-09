a93e33834034bd9469f6f4fd8d87b93319733f8d
vaishnavinkm <vaishnavinikam1999@gmail.com>
2025-03-09 14:38:04 -0400

    Getting the book details based on the auther

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,10 +21,17 @@ public_users.get('/isbn/:isbn',function (req, res) {
     res.send(books[isbn]);
  });
   
-// Get book details based on author
+// Task:3 Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+        let book = [];
+
+         Object.keys(books).forEach(i => {
+          if(books[i].author.toLowerCase() == author.toLowerCase()){
+               book.push(books[i]);
+            }
+    });
+        res.send(book);
 });
 
 // Get all books based on title
