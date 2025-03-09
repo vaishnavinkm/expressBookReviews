d83ee918fa46a37dc737b07184b3386ad8e78e8f
vaishnavinkm <vaishnavinikam1999@gmail.com>
2025-03-09 14:12:12 -0400

    Added route to get book details by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,15 +10,14 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
+// Task:1 Get the book list available in the shop
 public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books,null,4));
 });
 
-// Get book details based on ISBN
+// Task:2 Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
  });
   
 // Get book details based on author
